aab11405b154e0d39f8d317fd954083737e79970
Twfic <jrazur@gmail.com>
2014-02-23 14:28:42 -0500

    Waff testing commit

--- DungeonSecretary/src/com/example/dungeonsecretary/DungeonSecretary.java
+++ DungeonSecretary/src/com/example/dungeonsecretary/DungeonSecretary.java
@@ -15,6 +15,7 @@ public class DungeonSecretary extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		//Waff committ test
 		getMenuInflater().inflate(R.menu.dungeon_secretary, menu);
 		return true;
 	}
