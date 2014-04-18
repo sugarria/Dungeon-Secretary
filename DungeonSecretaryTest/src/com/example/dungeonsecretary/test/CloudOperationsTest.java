87a120631eddf0a88e080ed76670261b28fe8ca9
Eric <thepostit@gmail.com>
2014-04-17 21:14:29 -0400

    fixed unit tests

--- DungeonSecretaryTest/src/com/example/dungeonsecretary/test/CloudOperationsTest.java
+++ DungeonSecretaryTest/src/com/example/dungeonsecretary/test/CloudOperationsTest.java
@@ -7,8 +7,10 @@ import com.example.dungeonsecretary.cloud.CloudOperations;
 import com.example.dungeonsecretary.model.CharacterData;
 import com.example.dungeonsecretary.model.SheetFieldData;
 import com.example.dungeonsecretary.model.StatData;
+import com.example.dungeonsecretary.model.UserData;
 import com.example.dungeonsecretary.sql.DungeonDataSource;
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -24,17 +26,25 @@ public class CloudOperationsTest extends AndroidTestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 		context	= new RenamingDelegatingContext(getContext(), "test_");
-		
+		Parse.initialize(context, "E8H0nRddgfMBoHMya6GPBM4IVwZAnuqq4jZUzhzG", "ZQi4BMLnzucog6zPIEOzuMe7KMlTqZLqhvD2VcDY");
 		dbData = DungeonDataSource.TESTgetInstance(context, "testDB.db");
+		UserData user = new UserData();
+		user.setGoogleAccount("test@test.com");
+		user.setUserName("testy");
+		dbData.insertUser(user);
+		dbData.setCurrentUser("test@test.com", "testy");
 		Log.d("UnitTesting", "set up");
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
+		dbData.close();
+		context.deleteDatabase("testDB.db");
+		Log.d("UnitTesting", "tear down");
 	}
 
 	public void testSendCharacterToCloudStringStringStringStringListOfStatDataListOfSheetFieldDataBooleanBoolean() {
-		CloudOperations.sendCharacterToCloud("bring home", "testemail@ihatethis.com", "why not zoidberg", "the bacon", new ArrayList<StatData>(), new ArrayList<SheetFieldData>(), true, true);
+		CloudOperations.sendCharacterToCloud("bring home", "testemail@example.com", "why not zoidberg", "the bacon", new ArrayList<StatData>(), new ArrayList<SheetFieldData>(), true, true);
 		ParseQuery<ParseObject> charQuery = ParseQuery.getQuery("Character");
 		charQuery.whereEqualTo("Name", "bring home");
 		charQuery.whereEqualTo("Owner", "testemail@ihatethis.com");
@@ -47,7 +57,11 @@ public class CloudOperationsTest extends AndroidTestCase {
 					if (charList.size() > 0)
 					{
 						Log.d("UnitTesting", "character added");
-						
+						assertEquals("Character Name:", charList.get(0).getString("Name"), "bring home");
+						assertEquals("Character OwnerId:", charList.get(0).getString("Owner"), "testemail@example.com");
+						assertEquals("Character System:", charList.get(0).getString("System"), "the bacon");
+						assertEquals("Character Public:", charList.get(0).getBoolean("Public"), true);
+						assertEquals("Character Shared:", charList.get(0).getBoolean("Shared"), true);
 					}
 				}
 			}
@@ -64,6 +78,7 @@ public class CloudOperationsTest extends AndroidTestCase {
 		newChar.setSystem("woop woop woop woop woop");
 		newChar.setOwnerId(dbData.getCurrentUser().getId());
 		dbData.insertCharacter(newChar);
+		
 		CloudOperations.sendCharacterToCloud("why not zoidberg", dbData.getCurrentUser().getId(), "woop woop woop woop woop", true, true, context);	
 		ParseQuery<ParseObject> charQuery = ParseQuery.getQuery("Character");
 		charQuery.whereEqualTo("Name", "why not zoidberg");
@@ -77,7 +92,12 @@ public class CloudOperationsTest extends AndroidTestCase {
 					if (charList.size() > 0)
 					{
 						Log.d("UnitTesting", "character added");
-						
+						assertEquals("Character Name:", charList.get(0).getString("Name"), "why not zoidberg");
+						assertEquals("Character OwnerId:", charList.get(0).getString("Owner"), dbData.getCurrentUser().getId());
+						assertEquals("Character System:", charList.get(0).getString("System"), "woop woop woop woop woop");
+						assertEquals("Character Public:", charList.get(0).getBoolean("Public"), true);
+						assertEquals("Character Shared:", charList.get(0).getBoolean("Shared"), true);
+						// this has to be tested by a live user. it would be obvious if this failed because other users would not be able to find your character.
 					}
 				}
 			}
@@ -90,6 +110,21 @@ public class CloudOperationsTest extends AndroidTestCase {
 	public void testAddUserToCloud() {
 		CloudOperations.addUserToCloud(context);
 		Log.d("UnitTesting", "user added");
+		ParseQuery<ParseObject> userQuery = ParseQuery.getQuery("User");
+		userQuery.whereEqualTo("GPlusEmail", dbData.getCurrentUser().getGoogleAccount());
+		userQuery.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> userList, ParseException e) {
+				if (e == null) {
+					if (userList.size() > 0)
+					{
+						// this has to be tested by a live user. it would be obvious if this failed because most of the features of the app would fail.
+					}
+				}
+			}
+			
+		});
 	}
 
 	public void testAddCurrentUserAsBuddy() {
@@ -105,7 +140,6 @@ public class CloudOperationsTest extends AndroidTestCase {
 					if (friendList.size() > 0)
 					{
 						Log.d("UnitTesting", "character added");
-						
 					}
 				}
 			}
